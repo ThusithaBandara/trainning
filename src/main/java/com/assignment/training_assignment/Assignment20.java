c3f0ab8e24e8e80488b3a4c60c443d4b4395f8a1
ThusithaBandara <thusitharmtgk95@gmail.com>
2019-10-16 17:34:11 +0530

    get the System details

---
+++ src/main/java/com/assignment/training_assignment/Assignment20.java
@@ -0,0 +1,13 @@
+package com.assignment.training_assignment;
+
+public class Assignment20 {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.version"));
+		System.out.println(System.getProperty("user.name"));
+		
+		
+	}
+
+}
